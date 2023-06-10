7a254522cc627eb4f0774cb69dbc4070556f9caa
ghousalik97 <ghousali11@gmail.com>
2023-06-10 23:18:42 +0800

    removed custom error handler

--- app.js
+++ app.js
@@ -30,24 +30,3 @@ app.get("/", (req, res) => {
 
 
 
-
-
-
-
-app.use(function (req, res, next) {
-    next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "DEVPMENT" ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
-});
-
-
-
